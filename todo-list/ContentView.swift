d336fd29ee77c4c2cc375a5003661b35954053ce
Anna Bukreeva <anyaspb96@gmail.com>
2020-10-24 21:04:09 +0200

    Initial Commit

---
+++ todo-list/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  todo-list
+//
+//  Created by Anna Bukreeva on 24.10.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
